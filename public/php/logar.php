3410ef0e766673000df9278ce311667ee2e219dc
BADjuninho <127327398+BADjuninho@users.noreply.github.com>
2024-05-02 11:29:39 -0300

    main

--- public/php/logar.php
+++ public/php/logar.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once "../php/conexao.php";
 
 class AuthController {
@@ -6,6 +7,47 @@ class AuthController {
 
     public function __construct() {
         $this->conn = $GLOBALS['conn'];
+    }
+
+    public function login() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Verifique as credenciais do usuário no banco de dados
+            $login = $_POST["login"];
+            $senha = $_POST["senha"];
+
+            // Consulta SQL para verificar as credenciais
+            $query = "SELECT id_usuario, cargo, id_empresa_user, nome_usuario FROM usuarios WHERE login = ? AND senha = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bind_param("ss", $login, $senha);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows == 1) {
+                // Login bem-sucedido
+                $row = $result->fetch_assoc();
+
+                $query2 = "SELECT id_aluno FROM alunos WHERE id_usuario_aluno = ?";
+                $stmt2 = $this->conn->prepare($query2);
+                $stmt2->bind_param("i", $row['id_usuario']);
+                $stmt2->execute();
+                $result2 = $stmt2->get_result();
+                $row2 = $result2->fetch_assoc();
+
+                session_start();
+                $_SESSION['id_aluno'] = $row2['id_aluno'];
+                $_SESSION['login'] = $row['login'];
+                $_SESSION['nome_usuario'] = $row['nome_usuario'];
+                $_SESSION['id_usuario'] = $row['id_usuario'];
+                $_SESSION['cargo'] = $row['cargo'];
+                $_SESSION['id_empresa_user'] = $row['id_empresa_user'];
+
+                // Redirecione com base no cargo do usuário
+                $this->redirectUser($row['cargo']);
+            } else {
+                // Login inválido
+                echo "<script language='javascript' type='text/javascript'>alert('Credenciais inválidas!');window.location.href='/';</script>";
+            }
+        }
     }
 
     public function loginAutomatico($login, $senha) {
@@ -60,7 +102,6 @@ class AuthController {
                 break;
             default:
                 // Redirecione para uma página padrão ou mostre uma mensagem de erro
-                echo "<script language='javascript' type='text/javascript'>alert('Cargo inválido!');window.location.href='/';</script>";
                 break;
         }
         exit(); // Saia após o redirecionamento
@@ -69,13 +110,6 @@ class AuthController {
 
 // Uso do controlador para fazer login
 $authController = new AuthController();
+$authController->login();
 
-// Verifique se o método de requisição é POST e se os parâmetros de login foram enviados
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"]) && isset($_POST["senha"])) {
-    $login = $_POST["login"];
-    $senha = $_POST["senha"];
-    $authController->loginAutomatico($login, $senha);
-} else {
-    echo "<script language='javascript' type='text/javascript'>alert('Ocorreu um erro ao processar sua solicitação. Por favor, tente novamente.');window.location.href='/';</script>";
-}
 ?>
